a2565ea599b3c504091a89e6659a07fb1f459a66
ThinhNguyen <sorrenw@gmail.com>
2024-09-10 00:44:54 +0700

    Implemented chunk streaming from buffered response
    - Custom handler for LLM, to implement generator through queue and threading
    - GET StreamingResponse from FastAPI
    - Receive API response as chunks, then pass it to buffer
    - Retrieve the chunk from the buffer then update it on the UI
    Todo: remove apikeys, credentials from source code (maybe write in a json file, include it in .gitignore, then read it from there)

--- App/app/src/main/java/com/example/digitpredictor/ChatbotActivity.kt
+++ App/app/src/main/java/com/example/digitpredictor/ChatbotActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
+import android.text.Html
 import android.util.Log
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
@@ -13,12 +14,18 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import kotlinx.coroutines.channels.*
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
+import okio.Buffer
 import okio.ByteString
+import okio.Utf8
+import java.io.IOException
+import java.nio.charset.Charset
 
 class ChatbotActivity:ComponentActivity() {
     private lateinit var client: OkHttpClient
@@ -48,7 +55,7 @@ class ChatbotActivity:ComponentActivity() {
                         bottomMargin = 20 // marginBottom
                     }
                     textSize=25F
-                    text = "USER\n" + message
+                    text = Html.fromHtml("<font color=#00FF00>USER</font><br>" + message)
                     textAlignment = TextView.TEXT_ALIGNMENT_VIEW_END
                 }
 
@@ -59,17 +66,51 @@ class ChatbotActivity:ComponentActivity() {
                         bottomMargin = 20 // marginBottom
                     }
                     textSize = 25F
-                    text = "CHATBOT\n"
+                    text = Html.fromHtml("<font color=#0000FF>CHATBOT</font><br>")
                 }
                 chatbox.addView(chatbotView)
 
-                val listener = com.example.digitpredictor.WebSocketListener(this, chatbox, chatbotView)
-                webSocket = client.newWebSocket(request, listener)
+//                val listener = com.example.digitpredictor.WebSocketListener(this, chatbox, chatbotView)
+//                webSocket = client.newWebSocket(request, listener)
+//
+//                webSocket.send(message)
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                    .method("GET", null)
+                    .url("http://10.0.2.2:8000/chatbot/stream/?request=${message}")
+                    .build()
 
-                webSocket.send(message)
+                client.newCall(request).enqueue(object: Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        println("Failed to receive response: ${e.message}")
+                    }
+
+                    override fun onResponse(call:Call, response:Response){
+                        if (!response.isSuccessful) {
+                            println("Unexpected code: ${response.code}")
+                            return
+                        }
+
+                        response.body?.let{body->
+                            val source = body.source()
+                            val buffer = Buffer()
+                            while(!source.exhausted()){
+                                val readBytes = source.read(buffer, Long.MAX_VALUE)
+                                val line = buffer.readString(Charset.forName("UTF-8"))
+                                //val line = source.readByteString(4)
+                                Log.d("debug",line.toString())
+                                if(line != null)
+                                    runOnUiThread(Runnable {
+                                        chatbotView?.text = Html.fromHtml((chatbotView?.text.toString() + line).replace("CHATBOT", "<font color=#0000FF>CHATBOT</font><br>"))
+                                    })
+                            }
+                            body.close()
+                        }
+                    }
+                })
             }
             viewTextInput.setText("")
-            webSocket.close(1000, "done")
+//            webSocket.close(1000, "done")
         }
     }
 }
